9babbb82c27e6f75bb9399febae55a6e83632c8a
Duarte Oliveira <duartefmoliveira8@gmail.com>
2018-11-30 18:16:46 +0000

    Navbar completed

---
+++ Templates/shared-components/navbar/navbar-right/LoggedOut.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../assets/css/navbar/navbar-right/LoggedOut.css">
+
+<?php function loggedOut() { ?>
+  <div class="right-element register">
+    <a href="#" class="login-link">Register</a>
+  </div>
+
+  <div class="right-element login">
+    <a href="#" class="login-link">Login</a>
+  </div>
+<?php } ?>
