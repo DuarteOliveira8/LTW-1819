b4c5718d54f60948b4837e399dd72ff197d1da51
Duarte Oliveira <duartefmoliveira8@gmail.com>
2018-11-30 15:19:15 +0000

    small fixes

--- Templates/scenes/index.php
+++ Templates/scenes/index.php
@@ -3,14 +3,14 @@
   <head>
       <meta charset="utf-8">
       <title>Reddit</title>
-      <link href="../css/style.css" rel="stylesheet">
+      <link href="../assets/css/index.css" rel="stylesheet">
   </head>
 
   <body>
     <header>
-        <img src="../images/icon.png" alt="Reddit Icon" width="50px"/>
+        <img src="../assets/images/icon.png" alt="Reddit Icon" width="50px"/>
 
-        <h1><a href="index.html">Reddit Boards</a></h1>
+        <h1><a href="index.php">Reddit Boards</a></h1>
         <h2>Where fake news have no place!</h2>
 
     </header>
