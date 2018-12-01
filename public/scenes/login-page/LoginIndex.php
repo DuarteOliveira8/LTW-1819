06e0c3fa030d8f1343e062b650139a9a68d58026
Pedrocssg <pedrokraktuga@gmail.com>
2018-12-01 16:11:38 +0000

    Added login files

---
+++ public/scenes/login-page/LoginIndex.php
@@ -0,0 +1,18 @@
+<?php include('../../shared-components/input-component/InputIndex.php') ?>
+<link rel="stylesheet" href="../../assets/css/login/login.css">
+
+<div class="login-box">
+
+  <h2>Welcome back!</h2>
+
+  <div class="email">
+    <h3>Email</h3>
+    <?php input('email'); ?>
+  </div>
+
+  <div class="password">
+    <h3>Password</h3>
+    <?php input('password'); ?>
+  </div>
+
+</div>
---
+++ public/shared-components/input-component/InputIndex.php
@@ -0,0 +1,3 @@
+<?php function input($type) { ?>
+  <input class="login" type="<? $type ?>" name="email" required>
+<?php } ?>
