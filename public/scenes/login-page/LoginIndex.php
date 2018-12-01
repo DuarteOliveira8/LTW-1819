1fb570e617d42ad3c010b960bf0e59ca7e20b28d
Pedro Gonçalves <pedrokraktuga@gmail.com>
2018-12-01 19:27:58 +0000

    Fixed vertical alignment

--- public/scenes/login-page/LoginIndex.php
+++ public/scenes/login-page/LoginIndex.php
@@ -2,28 +2,30 @@
 <link rel="stylesheet" href="/css/login/login.css">
 <link rel="stylesheet" href="/css/input/input.css">
 
-<div class="login-box">
+<div class="container">
+  <div class="login-box">
 
-  <div class="loginLogo">
-    <img src="/images/webbit.png" alt="Webbit logo" height="75px" width="75px"/>
+    <div class="loginLogo">
+      <img src="/images/webbit.png" alt="Webbit logo" height="75px" width="75px"/>
+    </div>
+
+    <div class="loginWelcome">Log In</div>
+
+    <div class="createAcc">Don't have an account? Create account.</div>
+
+    <div class="email">
+      <?php input('email', "Email address:", "Insert your email address"); ?>
+    </div>
+
+    <div class="password">
+      <?php input('password', "Password:", "Insert your password"); ?>
+    </div>
+
+    <div class="submitLogin">
+      <button class="submitButton" type="submit">Log In </button>
+    </div>
+
+    </div>
+
   </div>
-
-  <div class="loginWelcome">Log In</div>
-
-  <div class="createAcc">Don't have an account? Create account.</div>
-
-  <div class="email">
-    <?php input('email', "Email address:", "Insert your email address"); ?>
-  </div>
-
-  <div class="password">
-    <?php input('password', "Password:", "Insert your password"); ?>
-  </div>
-
-  <div class="submitLogin">
-    <button class="submitButton" type="submit">Log In </button>
-  </div>
-
-  </div>
-
 </div>
