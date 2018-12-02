57e492e512d17f55980059e0afdf6ae62a375397
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-12-02 20:15:37 +0000

    Added router to bootstrap file

--- public/index.php
+++ public/index.php
@@ -1,19 +1,3 @@
 <?php
 require __DIR__ . '/../src/Bootstrap.php';
-
-$request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
-
-switch ($request_uri[0]) {
-  case '/':
-    require 'scenes/home/HomeIndex.php';
-    break;
-
-  case ('/login' || '/login/'):
-    require 'scenes/login/LoginIndex.php';
-    break;
-
-  default:
-    echo "<h1 style='text-align: center; padding-top: 100px;'>404 NOT FOUND</h1>";
-    break;
-}
 ?>
