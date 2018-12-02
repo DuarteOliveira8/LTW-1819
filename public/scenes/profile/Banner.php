21f1bf39dd835cb1fb6c969e2c05fbc853f3ba17
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-12-02 22:17:49 +0000

    Profile banner

---
+++ public/scenes/profile/Banner.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="/css/scenes/profile/Banner.css">
+
+<?php function getBanner() { ?>
+  <div class="banner-container">
+    <img src="/images/default-background.jpg" alt="" class="background-image">
+
+    <div class="basic-info">
+      <img src="/images/default-profile.png" alt="" class="profile-picture">
+
+      <div class="profile-name">
+        Firstname Lastname
+      </div>
+    </div>
+  </div>
+<?php } ?>
--- public/scenes/profile/ProfileIndex.php
+++ public/scenes/profile/ProfileIndex.php
@@ -1,10 +1,19 @@
 <?php
 include('shared-components/Header.php');
+include('shared-components/navbar/NavbarIndex.php');
+include('scenes/profile/Banner.php');
 include('shared-components/Footer.php');
 
 getHeader();
+getNavbar();
 ?>
 
-<h1>Hello Profile!</h1>
+<link rel="stylesheet" href="/css/scenes/profile/ProfileIndex.css">
+
+<div class="profile-container">
+  <?php
+  getBanner();
+  ?>
+</div>
 
 <?php getFooter(); ?>
