6eb3c02b736f421655b3f1cd434710937cce46bf
Duarte Oliveira <duartefmoliveira8@gmail.com>
2018-12-08 11:40:11 +0000

    indentatio fix

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -1,22 +1,25 @@
 <?php
 $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 $db = new PDO('sqlite:webbit.db');
+
 if ($request_uri[0][strlen($request_uri[0])-1] == '/')
   $path = substr($request_uri[0], 0, strlen($request_uri[0])-1);
 else
   $path = $request_uri[0];
+
 switch ($path) {
   case '':
     require 'scenes/home/HomeIndex.php';
     break;
+
   case '/login':
     require 'scenes/login/LoginIndex.php';
     break;
-    
+
   case '/profile':
     require 'scenes/profile/ProfileIndex.php';
     break;
-    
+
   case '/register':
     require 'scenes/register/RegisterIndex.php';
     break;
