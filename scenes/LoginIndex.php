903940a85264d242d47a7c4a2ce6fdf1b3e676dc
DuarteOliveira8 <duartefmoliveira8@gmail.com>
2018-12-09 00:28:44 +0000

    Login done

--- scenes/LoginIndex.php
+++ scenes/LoginIndex.php
@@ -1,38 +1,9 @@
-<link rel="stylesheet" href="/css/scenes/login/login.css">
-
 <?php
-include('shared-components/input/Input.php');
-include('shared-components/Header.php');
-include('shared-components/Footer.php');
+  include('templates/shared-components/Header.php');
+  include('templates/scene-templates/login/Login.php');
+  include('templates/shared-components/Footer.php');
 
-getHeader();
+  getHeader();
+  getLogin();
+  getFooter();
 ?>
-
-<div class="container">
-  <div class="login-box">
-
-    <div class="loginLogo">
-      <a href="/"><img src="/images/webbit-rabbit.png" alt="Webbit logo" height="75px" width="65px"/></a>
-    </div>
-
-    <div class="loginWelcome">Log In</div>
-
-    <div class="createAcc">Don't have an account? <a href="/register">Create account</a>.</div>
-
-    <form method="POST">
-      <div class="email">
-        <?php input('email', "Email address:", "email", "Insert your email address", true); ?>
-      </div>
-
-      <div class="password">
-        <?php input('password', "Password:", "password", "Insert your password", true); ?>
-      </div>
-
-      <div class="submitLogin">
-        <button class="submitButton" type="submit">Log In</button>
-      </div>
-    </form>
-  </div>
-</div>
-
-<?php getFooter(); ?>
