ef9ff4627cb56a95c4515a3a447516563a07d5a7
Pedro Gonçalves <pedrokraktuga@gmail.com>
2018-12-15 18:14:19 +0000

    Infinite scroll implemented

--- js/scenes/home/Home.js
+++ js/scenes/home/Home.js
@@ -4,6 +4,9 @@ import {getChannel} from '/js/shared-components/Channel.js';
 let mainChannels = document.getElementById('main-channels');
 let recentPosts = document.getElementById('recent-posts');
 
+let postOffset = 0;
+let requesting = false;
+
 let showMainChannels = new XMLHttpRequest();
 let showRecentPosts = new XMLHttpRequest();
 
@@ -38,11 +41,26 @@ showRecentPosts.onreadystatechange = function() {
                            response.data[i].comments,
                            response.data[i].avatar));
     }
+    requesting = false;
   }
 };
 
 showMainChannels.open("GET", "/api/home/main-channels", true);
 showMainChannels.send();
 
-showRecentPosts.open("GET", "/api/home/recent-posts", true);
-showRecentPosts.send();
+showRecentPosts.open("POST", "/api/home/recent-posts", true);
+let reqObj = {"offset":postOffset};
+let request = JSON.stringify(reqObj);
+showRecentPosts.send(request);
+
+document.addEventListener('scroll', function (event) {
+    if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && !requesting) {
+      requesting = true;
+      postOffset += 8;
+      reqObj = {"offset": postOffset};
+      request = JSON.stringify(reqObj);
+      showRecentPosts.open("POST", "/api/home/recent-posts", true);
+      showRecentPosts.setRequestHeader("Content-Type", "application/json");
+      showRecentPosts.send(request);
+    }
+});
