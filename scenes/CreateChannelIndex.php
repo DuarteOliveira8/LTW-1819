7a199f3fbb9aeb5e811faa87905b215e09af639f
Pedro Gonçalves <pedrokraktuga@gmail.com>
2018-12-15 11:35:52 +0000

    CreateChannelIndex structure fixed

--- scenes/CreateChannelIndex.php
+++ scenes/CreateChannelIndex.php
@@ -2,20 +2,16 @@
   include_once('includes/Session.php');
   include_once('templates/shared-components/Header.php');
   include_once('templates/shared-components/navbar/NavbarIndex.php');
-  include_once('templates/scene-templates/channel/CreateChannel.php');
+  include_once('templates/scene-templates/create-channel/CreateChannel.php');
   include_once('templates/shared-components/Footer.php');
 
   getHeader();
   getNavbar();
 ?>
+
 <link rel="stylesheet" href="/css/scenes/create-channel/CreateChannelIndex.css">
 
-<div class="container">
-  <?php
-    getCreateChannel();
-  ?>
-</div>
-
 <?php
+  getCreateChannel();
   getFooter();
 ?>
